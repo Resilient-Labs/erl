7789fe0d93d1ec0ba80607db8a06a03da2e76e12
Max <exavierjean@hotmail.com>
2016-05-19 12:12:35 -0400

    add erl.js file

---
+++ erl.js
@@ -0,0 +1,11 @@
+<script>
+		$.ajax({
+			  method: "GET",
+			  url: "https://script.google.com/macros/s/AKfycbzWYRx_FESE2jyD25-inifDfG8QORQMtlLi-6_XU7dtA61UXAhE/exec",
+			  data: {
+			    name: 'Youngens Packen',
+			    count: 'Money Stacken',
+			    result: 'Once You\'ve Get Up Out The Game, you Cant Get Back In!'
+			  }
+				})
+</script>
