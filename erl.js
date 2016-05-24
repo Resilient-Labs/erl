e257e45fb2b41c22d24dc28b9a0e6a4542fd25f0
Max <exavierjean@hotmail.com>
2016-05-24 10:53:26 -0400

    Updates to all files

--- erl.js
+++ erl.js
@@ -1,11 +1,9 @@
-<script>
-		$.ajax({
-			  method: "GET",
-			  url: "https://script.google.com/macros/s/AKfycbzWYRx_FESE2jyD25-inifDfG8QORQMtlLi-6_XU7dtA61UXAhE/exec",
-			  data: {
-			    name: 'Youngens Packen',
-			    count: 'Money Stacken',
-			    result: 'Once You\'ve Get Up Out The Game, you Cant Get Back In!'
-			  }
-				})
-</script>
+$.ajax({
+		  method: "GET",
+		  url: "https://script.google.com/macros/s/AKfycbzWYRx_FESE2jyD25-inifDfG8QORQMtlLi-6_XU7dtA61UXAhE/exec",
+		  data: {
+		    name: 'Juice Man',
+		    email: 'JuiceMan@TGOD.com',
+		    companyName: 'Taylor Gang Inc.
+		  }
+			})
